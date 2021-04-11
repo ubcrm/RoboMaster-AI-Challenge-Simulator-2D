5949ee213d7ac1d3ab5ed62b7dd20bb73a830e6c
James <james.rosstwo@gmail.com>
2021-04-10 23:21:55 -0700

    complete implementation of abstract gym functions

--- gym_rmaics/envs/rmaics_env.py
+++ gym_rmaics/envs/rmaics_env.py
@@ -1,43 +1,44 @@
 import gym
 from gym import error, spaces
 from gym import utils
-from gym.utils import seeding
 import logging
 
 from modules.rmaics import Rmaics
-
-from modules.rmaics import Rmaics
-from modules.actor import Actor
-import numpy as np
 
 logger = logging.getLogger(__name__)
 
 
 class RMAICSEnv(gym.Env, utils.ezpickle):
 
-    def __init__(self, num_agents=1):
+    def __init__(self, num_agents=1, num_steps=400):
         self.rmaics = Rmaics(agent_num=num_agents, render=False)
-        self.game = self.rmaics.game
 
         # observation space for now is just x, y of each agent
         self.observation_space = spaces.Discrete(2 * num_agents)
         # Currently just movement: [x_dir, y_dir]
         self.action_space = spaces.Discrete(2)
         self.iter = 0
+        self.num_steps = num_steps
+
+    @property
+    def game(self):
+        return self.rmaics.game
 
     def step(self, action):
         self._take_action(action)
         self.game.one_epoch()
         reward = self.rmaics.get_reward()
-
+        ob = self.rmaics.get_observation()
+        episode_over = self.iter > self.num_steps
         self.iter += 1
+        return ob, reward, episode_over, {}
 
     def _take_action(self, action):
         self.game.set_actions([action])
 
-
     def reset(self):
-        pass
+        self.rmaics.reset()
+        self.iter = 0
 
-    def render(self, mode='human'):
-        pass
+    def render(self, mode="human"):
+        self.game.draw()
